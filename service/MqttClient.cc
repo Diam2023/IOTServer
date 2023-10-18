d8a04a8b9a586d9df83f763d466a59e2c4255c5a
monoliths <monoliths-uni@outlook.com>
2023-10-18 20:07:19 +0800

    create document

---
+++ service/MqttClient.cc
@@ -0,0 +1,8 @@
+//
+// Created by 35691 on 10/9/2023.
+//
+
+#include "MqttClient.h"
+
+namespace mqtt {
+} // mqtt
---
+++ service/MqttClient.h
@@ -0,0 +1,16 @@
+//
+// Created by 35691 on 10/9/2023.
+//
+
+#ifndef IOTSERVER_MQTTCLIENT_H
+#define IOTSERVER_MQTTCLIENT_H
+
+namespace mqtt {
+
+class MqttClient {
+
+};
+
+} // mqtt
+
+#endif //IOTSERVER_MQTTCLIENT_H
