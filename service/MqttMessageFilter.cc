fa570bf4d0fd17a8b904e4603166f0ba23b699b0
monoliths <monoliths-uni@outlook.com>
2023-10-30 16:08:17 +0800

    添加debugCache選項

---
+++ service/MqttMessageFilter.cc
@@ -0,0 +1,8 @@
+//
+// Created by 35691 on 10/23/2023.
+//
+
+#include "MqttMessageFilter.h"
+
+namespace mqtt {
+} // mqtt
---
+++ service/MqttMessageFilter.h
@@ -0,0 +1,16 @@
+//
+// Created by 35691 on 10/23/2023.
+//
+
+#ifndef IOTSERVER_MQTTMESSAGEFILTER_H
+#define IOTSERVER_MQTTMESSAGEFILTER_H
+
+namespace mqtt {
+
+class MqttMessageFilter {
+
+};
+
+} // mqtt
+
+#endif //IOTSERVER_MQTTMESSAGEFILTER_H
