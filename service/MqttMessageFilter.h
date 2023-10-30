c4fd0a85ff776f8483a03d08927dc98ef7232e13
monoliths <monoliths-uni@outlook.com>
2023-10-30 16:09:21 +0800

    制定Mqtt消息過濾策略

--- service/MqttMessageFilter.h
+++ service/MqttMessageFilter.h
@@ -5,11 +5,39 @@
 #ifndef IOTSERVER_MQTTMESSAGEFILTER_H
 #define IOTSERVER_MQTTMESSAGEFILTER_H
 
+#include <QMqttClient>
+
 namespace mqtt {
+    class MqttFilter {
+    public:
+        virtual bool doFilter(const QMqttTopicName &) = 0;
 
-class MqttMessageFilter {
+        ~MqttFilter() = default;
+    };
 
-};
+    // 需求：
+    // 需要一個過濾類來過濾Topic是否被Mysql收錄，如果收錄了就pass
+    // 思考：使用Redis加快查詢速度
+    class DataBaseMqttTopicFilter : public virtual MqttFilter, QObject {
+    Q_OBJECT;
+
+    private:
+        const std::string defaultCacheName = "MqttTopicCache";
+    public:
+
+        DataBaseMqttTopicFilter();
+
+        bool doFilter(const QMqttTopicName &) override;
+
+        static DataBaseMqttTopicFilter &getInstance() {
+            static DataBaseMqttTopicFilter dbmtf;
+            return dbmtf;
+        }
+
+        void doSyncUpdate();
+
+        [[noreturn]] void cacheUpdateTask();
+    };
 
 } // mqtt
 
