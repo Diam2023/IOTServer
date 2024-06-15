6fb79932d3e403908c4c099a6e6b40c65c8fd8ee
Diam <monoliths-uni@outlook.com>
2024-06-15 11:19:59 +0800

    Use Arg To Launch

--- IOTServer/main.cc
+++ IOTServer/main.cc
@@ -16,10 +16,17 @@
 
 #include "DeviceMessageNotify.h"
 
+/**
+ * @brief Config path
+ *
+ */
+static std::string configFile = "../../config.yaml";
+
 static QSharedPointer<QTimer> publisherWorkerPtr;
 
 // Handler Exit Signal
-static void exitSignalHandler() {
+static void exitSignalHandler()
+{
     // Handle Exit Signal
     LOG_WARN << "Server Exiting...";
     mqtt::MqttClient::instance().stop();
@@ -27,7 +34,8 @@ static void exitSignalHandler() {
     QCoreApplication::exit();
 }
 
-static int runServer() {
+static int runServer()
+{
     using namespace std::chrono_literals;
 
     // Drogon Thread
@@ -37,7 +45,7 @@ static int runServer() {
         drogon::app().setTermSignalHandler(&exitSignalHandler);
 
         // Load config file
-        drogon::app().loadConfigFile("/install/IOTServer/config/config.yaml");
+        drogon::app().loadConfigFile(configFile);
         LOG_INFO << "Document Root: " << drogon::app().getDocumentRoot();
 
         // Get Customer Config
@@ -49,7 +57,8 @@ static int runServer() {
     auto result = QCoreApplication::exec();
 
     // If Drogon Thread Not Exit Yet.
-    if (drogonThread.joinable()) {
+    if (drogonThread.joinable())
+    {
         // Wait To Exit
         drogonThread.join();
     }
@@ -60,30 +69,48 @@ static int runServer() {
 }
 
 // Main Process
-int main(int argc, char **argv) {
+int main(int argc, char **argv)
+{
     // Qt Core Application
     QCoreApplication app(argc, argv);
+
+    if (argc >= 1)
+    {
+        // Use arg as config
+        configFile = argv[1];
+    }
 
     cq::CqMessageManager::getInstance();
 
     QTimer::singleShot(500, &app, [&app]() {
-        // 因为QMqttClient依赖于socket所以在不同线程中启动与发布会出现 qsocketnotifier: socket notifiers cannot be enabled or disabled from another thread 问题
-        // 但是QMqttClient在其他线程启动后不工作，status卡在Connecting也不抛出错误 所以很头疼
-        // https://forum.qt.io/topic/109138/qmqttclient-connection-not-made-from-another-thread 方案不适用
-        // 最后解决方案Client在QtEventLoop中启动 Publish也在事件循环中调用
-        mqtt::MqttClient::instance().registerNotifyCallback(cq::CqMqttDeviceMessageHandler::getInstance());
-        mqtt::MqttClient::instance().registerNotifyCallback(DeviceMessageNotifyHandler::getInstance());
-        mqtt::MqttClient::instance().registerNotifyCallback([](const auto &r){
+        // 因为QMqttClient依赖于socket所以在不同线程中启动与发布会出现
+        // qsocketnotifier: socket notifiers cannot be enabled or disabled from
+        // another thread 问题
+        // 但是QMqttClient在其他线程启动后不工作，status卡在Connecting也不抛出错误
+        // 所以很头疼
+        // https://forum.qt.io/topic/109138/qmqttclient-connection-not-made-from-another-thread
+        // 方案不适用 最后解决方案Client在QtEventLoop中启动
+        // Publish也在事件循环中调用
+        mqtt::MqttClient::instance().registerNotifyCallback(
+            cq::CqMqttDeviceMessageHandler::getInstance());
+        mqtt::MqttClient::instance().registerNotifyCallback(
+            DeviceMessageNotifyHandler::getInstance());
+        mqtt::MqttClient::instance().registerNotifyCallback([](const auto &r) {
             // DEBUG
-            LOG_INFO << "TOPIC: " << r.first.name().toStdString() << "DATA: " << r.second.toStyledString();
+            LOG_INFO << "TOPIC: " << r.first.name().toStdString()
+                     << "DATA: " << r.second.toStyledString();
         });
 
         // Load Mqtt Launch Mqtt start!
-        mqtt::MqttClient::instance().loadConfig(drogon::app().getCustomConfig()["mqtt"]).start();
+        mqtt::MqttClient::instance()
+            .loadConfig(drogon::app().getCustomConfig()["mqtt"])
+            .start();
 
         publisherWorkerPtr.reset(new QTimer(&app));
 
-        QObject::connect(publisherWorkerPtr.data(), &QTimer::timeout, &mqtt::MqttMessagePublisher::getInstance(),
+        QObject::connect(publisherWorkerPtr.data(),
+                         &QTimer::timeout,
+                         &mqtt::MqttMessagePublisher::getInstance(),
                          &mqtt::MqttMessagePublisher::workerOnce);
         publisherWorkerPtr->start(50);
     });
